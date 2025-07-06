b0cf250d7dea788410e7e32709ad08deffd746ee
Ajax-11Fatui <danmeilover43@gmail.com>
2025-07-06 00:51:06 -0300

    Adições no CSS e as modificações iniciais no JSON

---
+++ assets/Java Script/deuses.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+fetch('assets/json/deuses.json')
+.then(Response => response.json ())
+.then(response => deuses {
+    const container = document.createElement
+})
+}
