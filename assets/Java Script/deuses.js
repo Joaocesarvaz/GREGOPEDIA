6dbbacfdb3e93fea2adca5defc5272d8601376c3
João César Vaz <joaocesarvazocm@gmail.com>
2025-07-08 19:43:30 -0300

    corriji o Json e adicionei algumas fontes da web

--- assets/Java Script/deuses.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-
-fetch('assets/json/deuses.json')
-.then(Response => response.json ())
-.then(response => deuses {
-    const container = document.createElement
-})
-}
