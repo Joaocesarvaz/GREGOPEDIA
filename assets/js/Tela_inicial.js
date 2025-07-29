e3f93f6915c86e72657fbf60b914f43b7bb1cf03
Ajax-11Fatui <danmeilover43@gmail.com>
2025-07-29 19:55:37 -0300

    estilizei mais a tela inicial e adicionei um fundo de transição apos clicar no botao da tela inicial

--- assets/js/Tela_inicial.js
+++ assets/js/Tela_inicial.js
@@ -1,11 +1,35 @@
-const botao = document.getElementById('entrar');
+document.getElementById("entrar").addEventListener("click", function () {
+  const videocont = document.getElementById("video-container");
+  const video = document.getElementById("video-transicao");
 
-const transicao = document.getElementById('transicao');
+  document.querySelector(".capa").style.display = "none";
 
-botao.addEventListener('click', () => {
-      transicao.classList.add('ativo');
+  videocont.style.display = "block";
+  videocont.classList.remove("desaparecido");
+  document.body.style.overflow = "hidden";
+
+  video.currentTime = 0;
+  video.play().catch(err => console.error("Erro ao reproduzir:", err));
+
+  const limite = 4;
+  video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= limite) {
+      video.pause();
+      videocont.classList.add("desaparecido");
       setTimeout(() => {
         window.location.href = "../index.html";
-      }, 1500); 
-    });
-    
+      }, 000);
+    }
+  });
+});
+
+const gatilho = document.getElementById("gatilho");
+const ovo = document.getElementById("ovo");
+
+gatilho.addEventListener('mouseenter', () => {
+  ovo.style.display = 'block';
+});
+
+gatilho.addEventListener('mouseleave', () => {
+  ovo.style.display = 'none';
+});
